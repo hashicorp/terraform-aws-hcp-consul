8b24d55848f2138f73520f75e4d51895b44f4027
Chris Piraino <cpiraino@hashicorp.com>
2021-09-03 15:02:20 -0500

    WIP: EC2 HCP Consul and AWS example

---
+++ examples/hcp-ec2-demo/README.md
@@ -0,0 +1,5 @@
+# hcp-ec2-demo
+
+This example creates all of the AWS and HCP resources necessary for connecting a
+HCP Consul cluster to a Consul client on EC2.
+
---
+++ examples/hcp-ec2-demo/variables.tf
@@ -0,0 +1,47 @@
+/*
+ *
+ * Optional Variables
+ *
+ */
+
+variable "cluster_id" {
+  type        = string
+  description = "The name of your HCP Consul cluster"
+  default     = "hcp-consul-tf-cluster"
+}
+
+variable "region" {
+  type        = string
+  description = "The AWS region to create resources in"
+  default     = "us-west-2"
+}
+
+variable "hvn_id" {
+  type        = string
+  description = "The name of your HCP HVN"
+  default     = "hvn-terraform"
+}
+
+variable "hvn_cidr_block" {
+  type        = string
+  description = "The CIDR range to create the HCP HVN with"
+  default     = "172.25.32.0/20"
+}
+
+variable "enable_public_url" {
+  type        = bool
+  description = "A boolean that determines whether the Consul cluster has a public URL"
+  default     = true
+}
+
+variable "size" {
+  type        = string
+  description = "The HCP Consul size to use when creating a Consul cluster"
+  default     = null
+}
+
+variable "tier" {
+  type        = string
+  description = "The HCP Consul tier to use when creating a Consul cluster"
+  default     = "development"
+}
