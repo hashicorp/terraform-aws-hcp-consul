d0dcb3bbf42d8c358f032e69e880193f2cdaa415
Chris Piraino <cpiraino@hashicorp.com>
2021-09-13 13:49:56 -0500

    Implement root module (#1)
    The goal of the root module is to abstract the necessary AWS and HCP
    resources required to establish connectivity between Consul clients
    running on AWS with a HCP Consul cluster running in a HVN.
    
    The root module accepts 4 parameters:
    - hvn_id
    - vpc_id
    - route_table_ids
    - security_group_ids
    
    The root module will do the following:
    1. Create and accept a peering connection between the HVN and AWS VPC
    2. Configure the HVN to route network traffic to the AWS VPC's CIDR
    3. Configure the passed in route_table_ids to route traffic to the HVN's CIDR
    4. Configure ingress traffic rules on the security_group_ids to allow the HCP Consul servers to gossip to Consul clients

---
+++ variables.tf
@@ -0,0 +1,25 @@
+/*
+ *
+ * Required Variables
+ *
+ */
+
+variable "hvn_id" {
+  type        = string
+  description = "The ID of your HCP HVN"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "The ID of your AWS VPC"
+}
+
+variable "route_table_ids" {
+  type        = list(string)
+  description = "A list of route table IDs which should route to the the HVN's CIDR"
+}
+
+variable "security_group_ids" {
+  type        = list(string)
+  description = "A list of security group IDs which should allow inbound Consul client traffic"
+}
