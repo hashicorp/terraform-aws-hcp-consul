a75489a06cd3e499d233a1795b0b1163f831f43f
Hans Hasselberg <hans@hashicorp.com>
2021-09-23 14:24:22 +0200

    remove secrets

--- examples/hcp-ec2-demo/providers.tf
+++ examples/hcp-ec2-demo/providers.tf
@@ -14,8 +14,3 @@ terraform {
 provider "aws" {
   region = var.region
 }
-
-provider "hcp" {
-  client_id = "SPA8Uqgs7GvDSIttzDsJLZbW9KVgSWbK"
-  client_secret = "8qoE80dp_mTjuCY8YBh2s8kiE4kHdJK9RzBP0WLdMyrkLXpq9-PCAodMcMfS-LHX"
-}
