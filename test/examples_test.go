2274b3a65dff46bb25f3977f1ac11da5f6d102bd
Chris Piraino <cpiraino@hashicorp.com>
2021-10-05 13:49:40 -0500

    eks-demo: add dashboard and counting service to EKS example
    Updates the hcp-eks-client module to deploy with an ingress gateway.
    
    This commit also adds a new test for the EKS example.

--- test/examples_test.go
+++ test/examples_test.go
@@ -77,3 +77,60 @@ func TestTerraform_EC2DemoExample(t *testing.T) {
 	r.NoError(err)
 	resp.Body.Close()
 }
+
+func TestTerraform_EKSDemoExample(t *testing.T) {
+	r := require.New(t)
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../examples/hcp-eks-demo",
+		Vars: map[string]interface{}{
+			"cluster_id": "test-eks-example",
+		},
+		NoColor: true,
+	})
+
+	t.Cleanup(func() {
+		terraform.Destroy(t, terraformOptions)
+	})
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	aclToken := terraform.Output(t, terraformOptions, "consul_root_token")
+	consulURL := terraform.Output(t, terraformOptions, "consul_url")
+	parsedURL, err := url.Parse(consulURL)
+	r.NoError(err)
+
+	c := &api.Config{
+		Address: parsedURL.Host,
+		Scheme:  parsedURL.Scheme,
+		Token:   aclToken,
+	}
+	consul, err := api.NewClient(c)
+	r.NoError(err)
+
+	r.Eventually(func() bool {
+		svcs, _, err := consul.Catalog().Services(nil)
+		if err != nil {
+			t.Logf("failed to query Consul services: %s", err)
+			return false
+		}
+
+		// We expect 6 total services, 1 for the Consul service, 1 for the ingress
+		// gateway, and 2 each for counting and dashboard service.
+		if len(svcs) == 6 {
+			return true
+		}
+
+		var registered []string
+		for k := range svcs {
+			registered = append(registered, k)
+		}
+		t.Logf("unexpected number of services registered: %v", registered)
+		return false
+	}, 1*time.Minute, 5*time.Second)
+
+	dashboardURL := terraform.Output(t, terraformOptions, "dashboard_url")
+	resp, err := http.Get(dashboardURL)
+	// We really just care that the service is reachable
+	r.NoError(err)
+	resp.Body.Close()
+}
