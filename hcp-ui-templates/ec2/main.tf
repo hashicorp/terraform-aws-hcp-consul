3bb30bd1c0904c2975baf584e110985b831c82d1
Hans Hasselberg <me@hans.io>
2022-01-03 13:46:10 +0100

    show nomad uri (#26)

--- hcp-ui-templates/ec2/main.tf
+++ hcp-ui-templates/ec2/main.tf
@@ -85,8 +85,8 @@ output "consul_url" {
   value = hcp_consul_cluster.main.consul_public_endpoint_url
 }
 
-output "nomad_ec2_id" {
-  value = module.aws_ec2_consul_client.host_id
+output "nomad_url" {
+  value = "http://${module.aws_ec2_consul_client.host_dns}:8081"
 }
 
 output "hashicups_url" {
