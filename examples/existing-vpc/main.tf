2554dd06ab60ab92a2af8358e5bef1c785315512
Hans Hasselberg <hans@hashicorp.com>
2022-01-16 22:20:04 +0100

    fix existing-vpc example

--- examples/existing-vpc/main.tf
+++ examples/existing-vpc/main.tf
@@ -23,6 +23,8 @@ module "vpc" {
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   enable_dns_hostnames = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
 }
 
 output "vpc_id" {
