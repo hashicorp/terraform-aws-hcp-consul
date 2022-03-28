0eed21582c47c12f14c994f6097f413c66e63a55
JJTimmons <josh.timmons@hashicorp.com>
2022-03-28 13:17:26 -0400

    Apply ec2 change and module dep to templates

--- hcp-ui-templates/eks/main.tf
+++ hcp-ui-templates/eks/main.tf
@@ -119,7 +119,7 @@ resource "hcp_hvn" "main" {
 
 module "aws_hcp_consul" {
   source  = "hashicorp/hcp-consul/aws"
-  version = "~> 0.6.2"
+  version = "~> 0.6.1"
 
   hvn                = hcp_hvn.main
   vpc_id             = module.vpc.vpc_id
@@ -141,7 +141,7 @@ resource "hcp_consul_cluster_root_token" "token" {
 
 module "eks_consul_client" {
   source  = "hashicorp/hcp-consul/aws//modules/hcp-eks-client"
-  version = "~> 0.6.2"
+  version = "~> 0.6.1"
 
   cluster_id       = hcp_consul_cluster.main.cluster_id
   consul_hosts     = jsondecode(base64decode(hcp_consul_cluster.main.consul_config_file))["retry_join"]
@@ -161,7 +161,7 @@ module "eks_consul_client" {
 
 module "demo_app" {
   source  = "hashicorp/hcp-consul/aws//modules/k8s-demo-app"
-  version = "~> 0.6.2"
+  version = "~> 0.6.1"
 
   depends_on = [module.eks_consul_client]
 }
