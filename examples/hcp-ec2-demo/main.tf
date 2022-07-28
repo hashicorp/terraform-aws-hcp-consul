266de0443b7b5c1b22ebd10d42d74dbe591dc37c
Jona Apelbaum <jona@hashicorp.com>
2022-07-28 22:06:23 +0200

    change output and ui template script

--- examples/hcp-ec2-demo/main.tf
+++ examples/hcp-ec2-demo/main.tf
@@ -45,10 +45,29 @@ resource "hcp_consul_cluster_root_token" "token" {
   cluster_id = hcp_consul_cluster.main.id
 }
 
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "hcp_ec2" {
+  count      = local.ssh ? 1 : 0
+  key_name   = "hcp-ec2-key"
+  public_key = tls_private_key.ssh.public_key_openssh
+}
+
+resource "local_file" "ssh_key" {
+  count           = local.ssh ? 1 : 0
+  filename        = "${aws_key_pair.key_pair.key_name}.pem"
+  content         = tls_private_key.ssh.private_key_pem
+  file_permission = "400"
+}
+
 module "aws_ec2_consul_client" {
   source  = "hashicorp/hcp-consul/aws//modules/hcp-ec2-client"
   version = "~> 0.7.2"
 
+  ssh_keyname              = local.ssh ? aws_key_pair.key_pair.key_name : ""
   subnet_id                = module.vpc.public_subnets[0]
   security_group_id        = module.aws_hcp_consul.security_group_id
   allowed_ssh_cidr_blocks  = ["0.0.0.0/0"]
@@ -57,4 +76,6 @@ module "aws_ec2_consul_client" {
   client_ca_file           = hcp_consul_cluster.main.consul_ca_file
   root_token               = hcp_consul_cluster_root_token.token.secret_id
   consul_version           = hcp_consul_cluster.main.consul_version
+  install_demo_app         = local.install_demo_app
+  vpc_id                   = module.vpc.vpc_id
 }
