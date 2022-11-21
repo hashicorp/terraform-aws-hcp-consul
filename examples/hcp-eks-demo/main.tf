d7510b545e088ffb40c82b5daa18a7b22246643a
Riddhi Shah <riddhi@hashicorp.com>
2022-11-21 08:11:10 -0800

    Fix module sources

--- examples/hcp-eks-demo/main.tf
+++ examples/hcp-eks-demo/main.tf
@@ -87,9 +87,8 @@ resource "hcp_consul_cluster_root_token" "token" {
 }
 
 module "eks_consul_client" {
-  # source  = "hashicorp/hcp-consul/aws//modules/hcp-eks-client"
-  # version = "~> 0.8.9"
-  source = "../../modules/hcp-eks-client/"
+  source  = "hashicorp/hcp-consul/aws//modules/hcp-eks-client"
+  version = "~> 0.8.10"
 
   boostrap_acl_token = hcp_consul_cluster_root_token.token.secret_id
   cluster_id         = hcp_consul_cluster.main.cluster_id
@@ -106,10 +105,9 @@ module "eks_consul_client" {
 }
 
 module "demo_app" {
-  count = var.install_demo_app ? 1 : 0
-  # source  = "hashicorp/hcp-consul/aws//modules/k8s-demo-app"
-  # version = "~> 0.8.10"
-  source = "../../modules/k8s-demo-app/"
+  count   = var.install_demo_app ? 1 : 0
+  source  = "hashicorp/hcp-consul/aws//modules/k8s-demo-app"
+  version = "~> 0.8.10"
 
   depends_on = [module.eks_consul_client]
 }
